fe4d1a23a2d2cb4ba32b5d5c3d0b7b0c3c7bf4e5
Mariyan Georgiev <mariqn5000@gmail.com>
2023-10-27 16:11:47 +0300

    installed cloudinary

--- project/settings.py
+++ project/settings.py
@@ -8,7 +8,18 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
+
+
 """
+
+import cloudinary
+
+cloudinary.config(
+    cloud_name="dzkpnriaw",
+    api_key="549581289117547",
+    api_secret="Jwd3ECNeENCxeCcEE3LZyx1DY14"
+)
+DEFAULT_FILE_STORAGE='cloudinary_storage.storage.MediaCloudinaryStorage'
 import os
 from pathlib import Path
 
