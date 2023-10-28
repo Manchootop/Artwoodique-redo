89196d576a4cce324bda85e9f1a5347d53ad2157
Mariyan Georgiev <mariqn5000@gmail.com>
2023-10-28 19:05:35 +0300

    temp secret key exposure

--- project/settings.py
+++ project/settings.py
@@ -4,8 +4,10 @@ import cloudinary
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Read SECRET_KEY from the file
-with open('secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
+# if DEBUG:
+#     with open('secret_key.txt') as f:
+#         SECRET_KEY = f.read().strip()
+SECRET_KEY = 'django-insecure-=(!&olqxy6ni=d_7_)#x6#9rhs9(oad+@h%f1(+utjc-8k99a+'
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
