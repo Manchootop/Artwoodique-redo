d9e5d2d20f80cfa8a61fd04064f4069d6a08a30e
Mariyan Georgiev <mariqn5000@gmail.com>
2023-10-29 15:23:00 +0200

    redesign calalog/store pages

--- project/main/urls.py
+++ project/main/urls.py
@@ -5,7 +5,7 @@ from project.main import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
     path('catalog/', views.CatalogView.as_view(), name='catalog'),
-    path('catalog/<int:pk>/', views.ProductDetailsView.as_view(), name='catalog-details'),
+    path('details/<int:pk>/', views.ProductDetailsView.as_view(), name='catalog-details'),
     path('store/', views.CollectionView.as_view(), name='store'),
     path('faq/', views.FAQView.as_view(), name='faq'),
     path('contacts/', views.ContactsView.as_view(), name='contacts'),
