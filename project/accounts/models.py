ca5e81046b9902022557e60bb08f3eb08ebfee3e
Mariyan Georgiev <mariqn5000@gmail.com>
2024-02-11 12:35:39 +0200

    fixes

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import PermissionsMixin, User, Group, Permission
 from django.core.exceptions import ValidationError
 from django.core.validators import MinLengthValidator
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, Group, Permission
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 from project.accounts.managers import ArtwoodiqueUserManager
 
 
